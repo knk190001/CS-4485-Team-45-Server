10c326e00ec2a155bf7d96a98080b8f419a8a39c
Keshav Santhanam <62087234+KeshavSanthanam@users.noreply.github.com>
2024-04-25 02:35:39 -0500

    Update GamePlayController.java

--- src/main/java/edu/utdallas/cs4485/team45/server/GamePlayController.java
+++ src/main/java/edu/utdallas/cs4485/team45/server/GamePlayController.java
@@ -4,23 +4,38 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// adding the classes used?
+import edu.utdallas.cs4485.team45.server.entities.Card;
+import edu.utdallas.cs4485.team45.server.entities.GameEngine;
+import edu.utdallas.cs4485.team45.server.entities.GameState; 
+
 @RestController
 @RequestMapping("/api")
 public class GamePlayController {
+    private GameEngine gameEngine = new GameEngine();
 
-    @PostMapping("/game/playCard")
-    public String playCard(@RequestBody PlayCardRequest request) {
+    // card colors: red, blue, green, yellow, black 
+    // ##
+    // add player, add lobby, start game, play card, draw card
+    // add get game state function
+    @PostMapping("/game/playCard/{id}")
+    public String playCard(@PathVariable("id") int id, @RequestBody PlayCardRequest request) {
         // play card logic
+        Card.Color colorToChangeTo = request.getColorToChangeTo();
+        return gameEngine.playCard(id, colorToChangeTo);
     }
 
-    @PostMapping("/game/drawCard/{count}")
-    public String drawCard(@PathVariable("count") int count, @RequestBody DrawCardRequest request) {
+    @PostMapping("/game/drawCard")
+    public String drawCard(@RequestBody DrawCardRequest request) {
         // draw card logic
+        return gameEngine.drawCard();   
     }
 
     @PostMapping("/game/passTurn")
     public String passTurn(@RequestBody PassTurnRequest request) {
         // pass turn logic
+        gameEngine.switchToNextPlayer();
+        return gameEngine.makeGameState();
     }
 
     @PostMapping("/game/callUNO")
