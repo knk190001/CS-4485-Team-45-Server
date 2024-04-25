be44f9caf1c8cd45ad60eb375bc09467b14d0cda
Keshav Santhanam <62087234+KeshavSanthanam@users.noreply.github.com>
2024-04-25 02:35:29 -0500

    Update GameManagementController.java

--- src/main/java/edu/utdallas/cs4485/team45/server/GameManagementController.java
+++ src/main/java/edu/utdallas/cs4485/team45/server/GameManagementController.java
@@ -4,19 +4,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// adding other classes
+import edu.utdallas.cs4485.team45.server.entities.GameEngine;
+import edu.utdallas.cs4485.team45.server.entities.GameState;
+
 @RestController
 @RequestMapping("/api")
 public class GameManagementController {
+    private GameEngine gameEngine = new GameEngine();
 
     @PostMapping("/game/start")
     public String startGame(@RequestBody StartGameRequest request) {
         // game start logic
+        gameEngine.addPlayer(request.getUserName());
+        return gameEngine.startGame();
     }
 
     @PostMapping("/game/leave")
     public String leaveGame(@RequestBody LeaveGameRequest request) {
         // game leave logic
     }
+
+    @PostMapping("/game/getGameState")
+    public String getGameState(@RequestBody LeaveGameRequest request) {
+        // get game state logic
+        return gameEngine.makeGameState(); //##
+    }
+
+    
 
     // @GetMapping("/game/{gameId}")
     // public GameInfo getGameInfo(@PathVariable("gameId") String gameId) {
