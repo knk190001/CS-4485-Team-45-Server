647346788dcb4de204a5299e7d07e933be9c23ee
knk190001 <knk190001@utdallas.edu>
2024-04-29 14:42:59 -0500

    Disable CORS

---
+++ src/main/java/edu/utdallas/cs4485/team45/server/WebConfiguration.java
@@ -0,0 +1,14 @@
+package edu.utdallas.cs4485.team45.server;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
+    }
+}
